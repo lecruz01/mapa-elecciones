2b79034f16da7c590da209ad98ac2ed1488ca224
Luis Cruz <lcruzc@xcaret.com>
2019-05-06 09:28:32 -0500

    Agregar capa de datos de prueba

--- src/scripts/mapLoader.js
+++ src/scripts/mapLoader.js
@@ -1,11 +1,10 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibGVjcnV6MDEiLCJhIjoiY2pyY2t1aXJiMWV3bTQ0bndiZHFtZnd3dCJ9.T_AWHGpxthQg8DRhzuKTGg';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/navigation-guidance-day-v2',
-    center: [-99.666233, 19.294109],
-    zoom: 10,
-    maxZoom: 13,
-    minZoom: 8,
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: {lng: -99.63714410328947, lat: 19.301763904648325},
+    zoom: 7.5,
+    maxZoom: 13
     // scrollZoom: false
 });
 
@@ -31,12 +30,52 @@ var popupOffsets = {
     'left': [markerRadius, (markerHeight - markerRadius) * -1],
     'right': [-markerRadius, (markerHeight - markerRadius) * -1]
 };
-var popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
-    .setLngLat([-99.666233, 19.294109])
-    .setHTML("<h3>Toluca</h3><p><strong>Poblacion: </strong>15 mill</p>")
-    .addTo(map);
+// var popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
+//     .setLngLat([-99.666233, 19.294109])
+//     .setHTML("<h3>Toluca</h3><p><strong>Poblacion: </strong>15 mill</p>")
+//     .addTo(map);
 
 map.addControl(new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
+    flyTo: {
+        speed: 2
+    },
+    marker: {
+        color: 'red'
+    },
     mapboxgl: mapboxgl
 }));
+
+map.on('load', () => {
+    console.log('onmaploaded');
+    var json = (function() {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "../assets/data/CP_15Mex_v5.json",
+            'dataType': "json",
+            'success': function(data) {
+                json = data;
+                console.log('cargado');
+                map.addSource('codigos', {
+                    type: 'geojson',
+                    data: json
+                });
+                map.addLayer({
+                    "id": "codigoslayer",
+                    "type": "fill",
+                    "source": "codigos",
+                    "layout": {},
+                    "paint": {
+                        "fill-color": "#627BC1",
+                        "fill-opacity": 0.5
+                    }
+                });
+                console.log(map.getLayer('codigoslayer'));
+            }
+        });
+        return json;
+    })();
+    
+});
