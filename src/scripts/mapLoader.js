b2de3c8b647ef00faf63e767b8761a2e4df9312c
Luis Cruz <lcruzc@xcaret.com>
2019-06-27 12:52:23 -0500

    Cambios

--- src/scripts/mapLoader.js
+++ src/scripts/mapLoader.js
@@ -2,10 +2,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibGVjcnV6MDEiLCJhIjoiY2pyY2t1aXJiMWV3bTQ0bndiZ
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
-    center: {lng: -99.63714410328947, lat: 19.301763904648325},
+    center: { lng: -99.63714410328947, lat: 19.301763904648325 },
     zoom: 7.5,
     maxZoom: 13
-    // scrollZoom: false
+        // scrollZoom: false
 });
 
 var nav = new mapboxgl.NavigationControl();
@@ -30,10 +30,10 @@ var popupOffsets = {
     'left': [markerRadius, (markerHeight - markerRadius) * -1],
     'right': [-markerRadius, (markerHeight - markerRadius) * -1]
 };
-// var popup = new mapboxgl.Popup({ offset: popupOffsets, className: 'my-class' })
-//     .setLngLat([-99.666233, 19.294109])
-//     .setHTML("<h3>Toluca</h3><p><strong>Poblacion: </strong>15 mill</p>")
-//     .addTo(map);
+var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
 
 map.addControl(new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
@@ -47,7 +47,6 @@ map.addControl(new MapboxGeocoder({
 }));
 
 map.on('load', () => {
-    console.log('onmaploaded');
     var json = (function() {
         var json = null;
         $.ajax({
@@ -57,7 +56,6 @@ map.on('load', () => {
             'dataType': "json",
             'success': function(data) {
                 json = data;
-                console.log('cargado');
                 map.addSource('codigos', {
                     type: 'geojson',
                     data: json
@@ -72,10 +70,39 @@ map.on('load', () => {
                         "fill-opacity": 0.5
                     }
                 });
+
+                map.on('mouseenter', 'codigoslayer', function(e) {
+                    // Change the cursor style as a UI indicator.
+                    map.getCanvas().style.cursor = 'pointer';
+
+                    console.log(e.features);
+                    var coordinates = e.features[0].geometry.coordinates.slice();
+                    console.log(coordinates[0]);
+                    var description = e.features[0].properties.description;
+
+                    // Ensure that if the map is zoomed out such that multiple
+                    // copies of the feature are visible, the popup appears
+                    // over the copy being pointed to.
+                    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+                    }
+
+                    // Populate the popup and set its coordinates
+                    // based on the feature found.
+                    popup.setLngLat(coordinates[0])
+                        .setHTML(description)
+                        .addTo(map);
+                });
+
+                map.on('mouseleave', 'places', function() {
+                    map.getCanvas().style.cursor = '';
+                    popup.remove();
+                });
+
                 console.log(map.getLayer('codigoslayer'));
             }
         });
         return json;
     })();
-    
+
 });
